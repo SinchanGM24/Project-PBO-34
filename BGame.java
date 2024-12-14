2bd15c24447169676ef60bf0491a5a645b0d4ecb
Gusfren <148834513+Gusfren@users.noreply.github.com>
2024-12-14 23:52:32 +0800

    0.8
    0.8

---
+++ BGame.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.Image;
+
+// Update pada kelas BGame
+public class BGame {
+    private JLabel backgroundLabel;
+    private List<ImageIcon> defaultBackgrounds = new ArrayList<>();
+    private List<ImageIcon> highStrength20Backgrounds = new ArrayList<>();
+    private List<ImageIcon> highIntelligence20Backgrounds = new ArrayList<>();
+    private List<ImageIcon> highStrength40Backgrounds = new ArrayList<>();
+    private List<ImageIcon> highIntelligence40Backgrounds = new ArrayList<>();
+    private List<ImageIcon> highStrength60Backgrounds = new ArrayList<>();
+    private List<ImageIcon> highIntelligence60Backgrounds = new ArrayList<>();
+    private List<ImageIcon> highStrength80Backgrounds = new ArrayList<>();
+    private List<ImageIcon> highIntelligence80Backgrounds = new ArrayList<>();
+
+    public BGame(JLabel backgroundLabel) {
+        this.backgroundLabel = backgroundLabel;
+        loadBackgrounds();
+    }
+
+    private void loadBackgrounds() {
+        try {
+            defaultBackgrounds.add(new ImageIcon("Background.jpg"));
+            highStrength20Backgrounds.add(new ImageIcon("strength20_bg.png"));
+            highIntelligence20Backgrounds.add(new ImageIcon("intelligence20_bg.png"));
+            highStrength40Backgrounds.add(new ImageIcon("strength40_bg.png"));
+            highIntelligence40Backgrounds.add(new ImageIcon("intelligence40_bg.png"));
+            highStrength60Backgrounds.add(new ImageIcon("strength60_bg.png"));
+            highIntelligence60Backgrounds.add(new ImageIcon("intelligence60_bg.png"));
+            highStrength80Backgrounds.add(new ImageIcon("strength80_bg.png"));
+            highIntelligence80Backgrounds.add(new ImageIcon("intelligence80_bg.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Error loading backgrounds!");
+        }
+    }
+
+    public void updateBackground(int strength, int intelligence) {
+        ImageIcon selectedIcon;
+        if (strength > 80) {
+            selectedIcon = highStrength80Backgrounds.get(0);
+        } else if (strength > 60) {
+            selectedIcon = highStrength60Backgrounds.get(0);
+        } else if (strength > 40) {
+            selectedIcon = highStrength40Backgrounds.get(0);
+        } else if (strength > 20) {
+            selectedIcon = highStrength20Backgrounds.get(0);
+        } else if (intelligence > 80) {
+            selectedIcon = highIntelligence80Backgrounds.get(0);
+        } else if (intelligence > 60) {
+            selectedIcon = highIntelligence60Backgrounds.get(0);
+        } else if (intelligence > 40) {
+            selectedIcon = highIntelligence40Backgrounds.get(0);
+        } else if (intelligence > 20) {
+            selectedIcon = highIntelligence20Backgrounds.get(0);
+        } else {
+            selectedIcon = defaultBackgrounds.get(0);
+        }
+
+        // Update ukuran background agar sesuai dengan label
+        Image scaledImage = selectedIcon.getImage().getScaledInstance(backgroundLabel.getWidth(), backgroundLabel.getHeight(), Image.SCALE_SMOOTH);
+        backgroundLabel.setIcon(new ImageIcon(scaledImage));
+    }
+}
